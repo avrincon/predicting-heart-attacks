dc00c9c91fd109b4641f0cd2b9cbff9f68093a9c
Alan Rincon <>
2025-04-29 09:09:34 -0700

    add helper functions

---
+++ R/plot_pred_data.R
@@ -0,0 +1,46 @@
+#' Plot model predictions
+#'
+#' @param pred_data A data frame of new data to make model predictions on
+#' @param mod_data A data frame of the original data used to fit the model
+#' @param x_var Name of the x-axis variable
+#' @param x_lab Label for x-axis
+#' @param x_breaks_original Numeric vector of breaks for x variable in original
+#' @param x_int Value of x-intercept for vline
+#'   non-logged scale
+#'
+#' @returns A ggplot object
+plot_pred_data <- function(pred_data, 
+                           mod_data, 
+                           x_var, 
+                           x_lab, 
+                           x_breaks_original,
+                           x_int) {
+  x_var <- rlang::sym(x_var)
+  
+  pred_data |> 
+    ggplot(aes(x = !!x_var, y = predicted_prob)) +
+    geom_line(color = "blue", linewidth = 1) +
+    geom_jitter(
+      data = mod_data, 
+      aes(x = !!x_var, y = ifelse(result == "positive", 1, 0)), 
+      height = 0.02, alpha = 0.3, color = "orange"
+    ) +
+    scale_x_continuous(
+      x_lab,
+      breaks = log(x_breaks_original),
+      labels = x_breaks_original
+    ) +
+    geom_vline(
+      xintercept = log(x_int), 
+      linetype = "dashed", 
+      color = "red",
+      linewidth = 1
+    ) +
+    ylab("Probability of Positive Result") +
+    geom_hline(yintercept = c(0, 1), linetype = "dashed", color = "gray") +
+    theme_minimal() +
+    theme(
+      text = element_text(size = 15)
+    )
+   
+}
---
+++ R/predict_result.R
@@ -0,0 +1,48 @@
+#' Model Predictions
+#' 
+#' Get logistic model predictions on new data
+#'
+#' @param age Age in years
+#' @param gender Gender. One of "Female" or "Male".
+#' @param heart_rate Heart Rate in bpm 
+#' @param systolic_blood_pressure Systolic blood pressure
+#' @param diastolic_blood_pressure Diastolic blood pressure
+#' @param blood_sugar Blood sugar levels
+#' @param ck_mb Creatine kinase-MB (ng/mL)
+#' @param troponin Troponin (ng/mL)
+#' @param logistic_model Logistic model object
+#'
+#' @returns A tibble with predicted results based on data and logistic model
+predict_result <- function(
+    age,
+    gender,
+    heart_rate,
+    systolic_blood_pressure,
+    diastolic_blood_pressure,
+    blood_sugar,
+    ck_mb,
+    troponin,
+    logistic_model
+) {
+  
+  pred_data <- 
+    tibble(
+      age = age,
+      gender = gender,
+      heart_rate = heart_rate,
+      systolic_blood_pressure = systolic_blood_pressure,
+      diastolic_blood_pressure = diastolic_blood_pressure,
+      blood_sugar = blood_sugar,
+      ck_mb = ck_mb,
+      troponin = troponin
+    )
+  
+  pred_data$predicted_prob <- 
+    predict(
+      logistic_model, 
+      newdata = pred_data, 
+      type = "response"
+    )
+  
+  pred_data
+}
